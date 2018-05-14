2f1ead4a55621452f042ce5a4819062c1cf98009
Trung <trunglepham1202@gmail.com>
2018-05-15 02:26:39 +0700

    Trung 0update server 20180515 0226

---
+++ WebAPI_C2C/WebAPI_C2C/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPI_C2C.Global" Language="C#" %>
---
+++ WebAPI_C2C/WebAPI_C2C/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_C2C.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Phonenumber { get; set; }
+        public float Point { get; set; }
+    }
+}
