e2589858641b11ab27a8043ba4474536e0a01cf6
Trung <trunglepham1202@gmail.com>
2018-06-20 17:45:23 +0700

    Trung update 20180620

--- Web_APIC2C/Web_APIC2C/Controllers/RatingController.cs
+++ Web_APIC2C/Web_APIC2C/Controllers/RatingController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 using Web_APIC2CDataAccess;
 
 namespace Web_APIC2C.Controllers
@@ -25,5 +27,66 @@ namespace Web_APIC2C.Controllers
                 return entities.Ratings.FirstOrDefault(e => e.RatingID == id);
             }
         }
+
+        // POST api/rating
+        public void Post([FromBody]string value)
+        {
+
+
+
+        }
+
+        /***
+         * public int RatingID { get; set; }
+         * public string RatingUserrating { get; set; }
+         * public string RatingUserrated { get; set; }
+         * public double RatingPoint { get; set; }
+         * public System.DateTime RatingDate { get; set; }****/
+        //User will submit His phone , Target Phone , Rating.
+        public string AddRating(Rating currentInput)
+        {
+            String UserRatingPhone = "01285799350";
+            String UserRatedPhone = " 0962518088";
+            using (C2CthesisEntities entities = new C2CthesisEntities())
+            {
+                Rating newRating = new Rating();// make object of table
+                newRating.RatingUserrating = UserRatingPhone;
+                newRating.RatingUserrated = UserRatedPhone;
+                newRating.RatingPoint = 5;
+                newRating.RatingDate = System.DateTime.Now;
+                entities.Ratings.Add(newRating);
+                entities.SaveChanges();
+            }
+
+            return "Done";
+        }
+
+        [ResponseType(typeof(Rating))]
+        public async Task<IHttpActionResult> PostRating(String newRatingString)
+        {
+
+            String UserRatingPhone = "01285799350";
+            String UserRatedPhone = "0962518088";
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Rating newRating = new Rating();// make object of table
+            using (C2CthesisEntities entities = new C2CthesisEntities())
+            {
+                
+                newRating.RatingUserrating = UserRatingPhone;
+                newRating.RatingUserrated = UserRatedPhone;
+                newRating.RatingPoint = 5;
+                newRating.RatingDate = System.DateTime.Now;
+                entities.Ratings.Add(newRating);
+                await entities.SaveChangesAsync();
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = newRating.RatingID }, newRating);
+        }
+
+
     }
 }
--- Web_APIC2C/Web_APIC2C/Controllers/UsersController.cs
+++ Web_APIC2C/Web_APIC2C/Controllers/UsersController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 using Web_APIC2CDataAccess;
 
 namespace Web_APIC2C.Controllers
@@ -18,12 +20,28 @@ namespace Web_APIC2C.Controllers
             }
         }
 
-        public User Get(String phone)
+        public String Get(String phone)
         {
             using (C2CthesisEntities entities = new C2CthesisEntities())
             {
-                return entities.Users.FirstOrDefault(e => e.UserPhonenumber == phone);
+                return entities.Users.FirstOrDefault(e => e.UserPhonenumber == phone).UserPoint.ToString();
             }
+        }
+
+        [ResponseType(typeof(User))]
+        public async Task<IHttpActionResult> PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (C2CthesisEntities entities = new C2CthesisEntities())
+            {
+                entities.Users.Add(user);
+                await entities.SaveChangesAsync();
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = user.UserPhonenumber }, user);
         }
     }
 }
